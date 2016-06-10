5d2c8260b433b93066c012d22f03646e905d2ccb
Shradha Pulla <pullashradha@gmail.com>
2016-06-10 07:54:14 -0700

    Move results div inside the jumbotron and container divs to keep formatting the same when showing results, add a .slideUp jQuery command in place of .show for the results in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,18 @@
 //User Interface Logic
-
 $(document).ready(function() {
   $("form#ping-pong").submit(function(event) {
     event.preventDefault();
     var userInput = parseInt($("input#number-input").val());
 
     $("#ping-pong-result").text(result);
-    $("#result").show();
+    $("#result").slideUp();
+    $(".jumbotron").hide();
   });
 });
 
 
-//Business End Logic
 
+//Business End Logic
 var equation = function(userInput) {
 
 }
